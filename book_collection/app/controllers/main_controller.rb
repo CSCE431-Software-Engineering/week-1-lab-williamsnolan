e5aa76488b93225bb7c90a03e7fa71e162a5d042
williamsnolan <104931000+williamsnolan@users.noreply.github.com>
2024-01-29 16:20:59 -0600

    Added some routes and controllers for testing

---
+++ book_collection/app/controllers/main_controller.rb
@@ -0,0 +1,14 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def hello
+    redirect_to(action: 'index')
+  end
+
+
+
+end
--- book_collection/config/routes.rb
+++ book_collection/config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "main#index"
+
+  match 'about', to: "main#about", via: :get
+  match 'hello', to: "main#hello", via: :get
+
+
+  get 'main/index'
+  get 'books/index'
+  get 'books/new'
+  get 'books/edit'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
